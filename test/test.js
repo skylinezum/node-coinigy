4873216e5afa7d92888649ab70f4f0c27f3b31a1
wenhou85 <wenhou@gmail.com>
2016-08-11 00:17:29 -0700

    added a few tests

--- test/test.js
+++ test/test.js
@@ -25,11 +25,52 @@ describe('Coinigy API', function () {
       return coinigy.activity()
       .then(function (b) {
         b.should.have.all.keys('data', 'notifications');
+        b.open_order.should.have.all.keys('exch_name');
       })
       .catch(function (err) {
         should.not.exist(err);
       });
     });
   });
-
+  describe('accounts', function () {
+    it('lists all accounts', function () {
+      return coinigy.accounts()
+      .then(function (b) {
+        b.should.have.all.keys('data', 'notifications');
+        should.not.exist(b.notifications);
+        //b.open_order.should.have.all.keys('exch_name');
+      })
+      .catch(function (err) {
+        should.not.exist(err);
+      });
+    });
+  });
+  describe('balances', function () {
+    it('lists all account balance', function () {
+      return coinigy.balances()
+      .then(function (b) {
+        b.should.have.all.keys('data', 'notifications');
+        should.not.exist(b.notifications);
+        //b.open_order.should.have.all.keys('exch_name');
+      })
+      .catch(function (err) {
+        should.not.exist(err);
+      });
+    });
+  });
+  describe('orders', function () {
+    it('lists all orders', function () {
+      return coinigy.orders()
+      .then(function (b) {
+        b.should.have.all.keys('data', 'notifications');
+        should.not.exist(b.notifications);
+        //b.open_order.should.have.all.keys('exch_name');
+      })
+      .catch(function (err) {
+        should.not.exist(err);
+      });
+    });
+  });
+  //How do I test post with a specific body? IE: auth_ids = 1234 in post for Refresh Balance
+  //http://docs.coinigy.apiary.io/#reference/account-functions/refresh-balance/refreshbalance
 });
